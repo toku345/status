5ecf86c97c787a2340db5600b7247e1f62442026
Fumitaka Tokumitsu <toku345@gmail.com>
2019-10-21 17:30:24 +0900

    Add Toku345-Status Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Toku345StatusComponent } from './toku345-status/toku345-status.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Toku345StatusComponent
+    Toku345StatusComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
