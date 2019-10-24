44ca1ddcd72484b1f213bcb0a6eaaf671e676c89
Fumitaka Tokumitsu <toku345@gmail.com>
2019-10-24 15:02:52 +0900

    prettierがきれいにしてくれた

--- src/app/toku345-status/toku345-status.component.ts
+++ src/app/toku345-status/toku345-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 export interface Toku345Status {
   name: string;
@@ -6,21 +6,19 @@ export interface Toku345Status {
 }
 
 @Component({
-  selector: 'app-toku345-status',
-  templateUrl: './toku345-status.component.html',
-  styleUrls: ['./toku345-status.component.css']
+  selector: "app-toku345-status",
+  templateUrl: "./toku345-status.component.html",
+  styleUrls: ["./toku345-status.component.css"]
 })
 export class Toku345StatusComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'status'];
+  displayedColumns: string[] = ["name", "status"];
   toku345Statuses: Toku345Status[] = [
-    { name: '体力', status: 'operational' },
-    { name: '集中力', status: 'warning' },
-    { name: 'コミュニケーション力', status: 'alert' },
+    { name: "体力", status: "operational" },
+    { name: "集中力", status: "warning" },
+    { name: "コミュニケーション力", status: "warning" }
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
