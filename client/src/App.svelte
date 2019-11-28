603713cfcd13352868a50d5f05e51a5fccea61a8
Fumitaka Tokumitsu <toku345@gmail.com>
2019-11-28 13:24:25 +0900

    Add Header & Confetti components

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
-  export let name;
+  import Header from "./Header.svelte";
+  import Confetti from "./Confetti.svelte";
 </script>
 
 <style>
@@ -10,13 +11,6 @@
     margin: 0 auto;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
   @media (min-width: 640px) {
     main {
       max-width: none;
@@ -25,10 +19,6 @@
 </style>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+  <Header />
+  <Confetti />
 </main>
---
+++ client/src/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
+
+<h1>toku345 status</h1>
