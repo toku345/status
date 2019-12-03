789dea8ff10196895c67e43af5b0fa2bc6d23c3f
Fumitaka Tokumitsu <toku345@gmail.com>
2019-12-03 12:49:14 +0900

    Update Whoami component

--- client/src/Whoami.svelte
+++ client/src/Whoami.svelte
@@ -6,11 +6,34 @@
     background-image: linear-gradient(160deg, #6c52d9 0%, #9b8ae6 127%);
     color: #fff;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
     font-size: 18px;
     font-weight: bold;
   }
+
+  img {
+    width: 85%;
+    margin: 20px 0 0;
+    border-radius: 50%;
+  }
+
+  .name {
+    font-size: 30px;
+    margin: 20px 0 0;
+    text-align: center;
+  }
+
+  .profession {
+    font-size: 18px;
+    text-align: center;
+  }
 </style>
 
-<div class="whoami">WHOAMI</div>
+<div class="whoami">
+  <img src="/avatar.jpg" alt="toku345" />
+  <div>
+    <div class="name">toku345</div>
+    <div class="profession">Web Programmer</div>
+  </div>
+</div>
