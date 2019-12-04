f6052120358a4c2bac1dc4e0e62cd711a0b0cade
Fumitaka Tokumitsu <toku345@gmail.com>
2019-12-04 13:02:50 +0900

    Update Header style

--- client/src/Header.svelte
+++ client/src/Header.svelte
@@ -1,15 +1,11 @@
-<script>
-
-</script>
-
 <style>
   header {
+    width: 100%;
     color: #9e01f7;
     font-weight: 100;
     font-size: 2em;
     text-align: center;
-    margin: 0;
-    padding-bottom: 10px;
+    margin: 0 0 10px;
     border-bottom: solid 1px;
   }
 </style>
