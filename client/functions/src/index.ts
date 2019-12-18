ffd092c619f00a524d155e18761dd63deca2a20f
Fumitaka Tokumitsu <toku345@gmail.com>
2019-12-19 06:37:44 +0900

    Add a simple `Slack Event Subscriptions` function

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -2,7 +2,18 @@ import * as functions from "firebase-functions";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+export const webhook = functions
+  .region("asia-northeast1")
+  .https.onRequest((request, response) => {
+    const payload = request.body;
+
+    if (payload.type === "url_verification") {
+      response.status(200).json({ challenge: payload.challenge });
+    } else {
+      if (payload.event && payload.event.type === "message") {
+        console.log(payload.event);
+      }
+      response.status(200).send("OK");
+    }
+  });
